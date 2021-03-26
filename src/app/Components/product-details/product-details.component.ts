8da2a728ac835cae8fb2d86f929d9e8b0cad9f4d
Daniel Santa <daniel.santar@udea.edu.co>
2021-03-25 22:57:41 -0500

    add atributes to product details

--- src/app/Components/product-details/product-details.component.ts
+++ src/app/Components/product-details/product-details.component.ts
@@ -10,20 +10,44 @@ import { MLApiService } from 'src/app/Services/mlapi.service';
 export class ProductDetailsComponent implements OnInit {
   item: any;
   flag: boolean = false;
-  constructor(private router: ActivatedRoute, private mlService: MLApiService, private navrouter: Router) {
+  seller: any;
+  shipping: boolean[] = [];
+  discount: number;
+  description: string = "";
+  constructor(
+    private router: ActivatedRoute,
+    private mlService: MLApiService,
+    private navrouter: Router
+  ) {
     this.mlService
       .getItemById(this.router.snapshot.paramMap.get('id'))
       .subscribe((data) => {
         this.item = data[0]['body'];
-        console.log(this.item);
-        this.flag = true;
+        this.discount = Math.round(
+          Math.abs((this.item.original_price * 100) / this.item.price - 100)
+        );
+        this.getSeller();
+        this.getDetails();
       });
   }
 
   ngOnInit(): void {}
   search(event: any) {
     if (event !== '') {
-      this.navrouter.navigate(['/search'], {queryParams: {search: event }});
+      this.navrouter.navigate(['/search'], { queryParams: { search: event } });
     }
+  }
+  getSeller() {
+    this.shipping.push(this.item['shipping']['free_shipping']);
+    const element = this.item['seller_id'];
+    this.mlService.getSeller(element).subscribe((data) => {
+      this.seller = data;
+      this.flag = true;
+    });
+  }
+  getDetails() {
+    this.mlService.getDescription(this.item['id']).subscribe((data) => {
+      this.description = data.plain_text
+    });
   }
 }
